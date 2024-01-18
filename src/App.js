1ea212c5339dfc78ddec18180159b4e05639ceaa
protacik <protacik@gmail.com>
2024-01-17 19:52:54 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import { TodoWraapper } from './components/TodoWrapper';
+
+function App() {
+  return (
+    <div className="App">
+      <TodoWraapper />
+    </div>
+  );
+}
+
+export default App;
